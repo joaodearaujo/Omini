7ae8b42b4653d20a4bf47d00932b6f2288f1c660
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-03-11 12:09:47 -0300

    feat(ui): update Tailwind styles and rename 'Menu' to 'Sidebar'

---
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../ui/Navbar/Navbar"
+
+const Menu = () => {
+
+    return (
+        <div className="h-full w-3xs border-r-2 border-[#c7c7c7]/40 flex flex-col items-center ">
+            <h1 className="text-center m-8 font-bold text-[54px]">Omini</h1>
+            <Navbar/ >
+            
+        </div>
+    )
+}
+
+export default Menu
