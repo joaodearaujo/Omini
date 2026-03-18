0c5e932bdea10655d81fa3afdc7ec0ac7240a0ec
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-03-18 13:43:00 -0300

    refactor(CATEGORY_MAP): add iconColor and bgColor props

--- frontend/src/Utilities/CategoryMap.ts
+++ frontend/src/Utilities/CategoryMap.ts
@@ -1,10 +1,11 @@
  import { UtensilsCrossed, HeartPlus, Bus, Lightbulb, Armchair} from 'lucide-react'
 import type { LucideIcon } from "lucide-react";
 
- export const CATEGORY_MAP: Record<string, LucideIcon> = {
-  Food: UtensilsCrossed,
-  Health: HeartPlus,
-  Transport: Bus,
-  Essentials: Lightbulb,
-  Lifestyle: Armchair
-};
+ export const CATEGORY_MAP: Record<string, {icon: LucideIcon, iconColor: string, bgColor: string}> = {
+
+  Food: {icon: UtensilsCrossed, iconColor: 'text-[#FF6B35]', bgColor: '#FFB38A'},
+  Health: {icon: HeartPlus, iconColor: 'text-[#00ED64]',bgColor: '#98FB98'},
+  Transport: {icon: Bus,iconColor: 'text-[#1E90FF]', bgColor: '#87CEFA'},
+  Essentials: {icon: Lightbulb, iconColor: 'text-[#FFD700]',bgColor: '#FDFD96'},
+  Lifestyle: {icon: Armchair, iconColor: 'text-[#BA55D3]',bgColor: '#C3B1E1'}
+ };
