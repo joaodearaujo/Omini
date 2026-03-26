138cb2b06c86340f7c1869c20dbf097371da7cf2
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-03-26 14:54:45 -0300

    feat(TransactionItem): finish component syle

--- frontend/src/pages/MyCards/components/TransactionsItem.tsx
+++ frontend/src/pages/MyCards/components/TransactionsItem.tsx
@@ -1,22 +1,23 @@
 import type { TransactionItemProps } from "../Mycards.type";
-import { GlassWaterIcon } from "lucide-react";
+import {  HelpCircle } from "lucide-react";
+import { CATEGORY_MAP } from "../../../utils/CategoryMap";
 
 const STYLE = {
-    conatainer: 'w-full h-[60px] flex justify-between items-center px-4 bg-white hover:bg-[#f9fafb] rounded-xl cursor-default',
-    icon: {iconSize: 20, iconBackground: 'bg-[#f3f4f6] w-[50px] h-[50px] rounded-full p-2 flex items-center justify-center', iconClass: 'text-[#404040]'},
+    conatainer: 'w-full h-[60px] flex justify-between items-center p-4 bg-white hover:bg-[#f9fafb] rounded-xl cursor-default',
+    icon: {iconSize: 20, iconBackground: 'bg-[#f3f4f6] w-[35px] h-[35px] rounded-full shadow-md flex items-center justify-center', iconClass: 'text-[#404040]'},
     receiver: 'font-medium',
-    info: 'font-light',
-    amount: 'font-medium',
+    info: 'font-light text-[#6a7282]',
+    status: 'capitalize text-[#008236]',
+    amount: 'font-medium text-[#404040]',
 }
 
 const TransactionItem = ({transaction}: TransactionItemProps) => {
 
-  const Icon = GlassWaterIcon;
+  const Icon = CATEGORY_MAP[transaction.category].icon || HelpCircle;
 
   return (
     <div className={STYLE.conatainer}>
-      <div className="flex gap-4">
-
+      <div className="flex w-fit items-center justify-between gap-4">
           <span className={STYLE.icon.iconBackground}>
               <Icon size={STYLE.icon.iconSize} className={STYLE.icon.iconClass} />
           </span>
@@ -28,7 +29,15 @@ const TransactionItem = ({transaction}: TransactionItemProps) => {
           </div>
       </div>
 
-      <span className={STYLE.amount}>{`R$ ${transaction.amount}`}</span>
+      <div className="flex w-[200px] items-center justify-between gap-2">
+        <div className="flex items-center justify-between gap-2 bg-[#dbfce7] px-2 rounded-2xl">
+          <div className="w-[6px] h-[6px] bg-[#008236] shadow-sm shadow-[#18a352] rounded-full"></div>
+
+          <small className={STYLE.status}>{transaction.status}</small>
+        </div>
+
+        <span className={STYLE.amount}>{`R$ ${transaction.amount}`}</span>
+      </div>
     </div>
 )
 }
