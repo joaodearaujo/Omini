50150655a746a7ada8f1248018325d15ace3194e
João Pedro de Araújo <joao.pedrocdearaujo@gmail.com>
2026-04-03 22:11:55 -0300

    feat(Transactions): apply atomizing

---
+++ omini-frontend/src/pages/Transactions/components/TransactionsModal.tsx
@@ -0,0 +1,28 @@
+import FloatingCard from "../../../components/ui/FloatingCard/FloatingCard";
+import { SquareArrowRightExit } from "lucide-react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const TransactionModal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-[1000] bg-black/50 flex items-center justify-center  p-4">
+      <FloatingCard style={{ width: '100%', maxWidth: '500px', padding: '40px', position: 'relative' }}>
+        <button 
+          className="absolute right-6 top-6 cursor-pointer text-zinc-400 hover:text-black transition" 
+          onClick={onClose}
+        >
+          <SquareArrowRightExit size={24} />
+        </button>
+        {children}
+      </FloatingCard>
+    </div>
+  );
+};
+
+export default TransactionModal
