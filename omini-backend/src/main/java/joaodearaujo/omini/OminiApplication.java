2bf5be4eccf2648863d4a71c0ceb92841f3cf683
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-05-20 14:22:08 -0300

    chore(backend): initialize Spring Boot project structure

---
+++ omini-backend/src/main/java/joaodearaujo/omini/OminiApplication.java
@@ -0,0 +1,13 @@
+package joaodearaujo.omini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OminiApplication.class, args);
+	}
+
+}
---
+++ omini-backend/src/test/java/joaodearaujo/omini/OminiApplicationTests.java
@@ -0,0 +1,13 @@
+package joaodearaujo.omini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
