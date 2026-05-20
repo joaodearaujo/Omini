8efd889cb18bd4af2d320f2d6211a78cb4429671
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-05-20 17:34:59 -0300

    feat(repository): create TransactionRepository

---
+++ omini-backend/src/main/java/joaodearaujo/omini/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package joaodearaujo.omini.repository;
+
+import joaodearaujo.omini.domain.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+}
